4042a2e52d48136430fab5a6e3557d3ccca45193
Viktor Malmedal <viktormalmedal@gmail.com>
2021-06-01 10:47:29 +0200

    osx setup

---
+++ .config/os-x-setup.sh
@@ -0,0 +1,41 @@
+xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# change alfred to use command space instead of spotlight
+brew install --cask alfred
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask brave-browser
+
+brew install git
+brew install neovim
+brew install tmux
+brew install zsh
+
+
+chsh -s /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+#install font https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
+
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Vim mode
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH/custom/plugins/zsh-vi-mode
+
+# nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+# add .zshrc file from github
+source ~/.zshrc
+
+
+
